981809240dd3b9d3afc2d462823e3ae4944737fa
Akshay.k <akshaykvl90@gmail.com>
2024-11-08 21:45:21 +0530

    settings

--- spot_your_stay/settings.py
+++ spot_your_stay/settings.py
@@ -84,23 +84,23 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'spoturstay_db',
-#         'USER': 'django_user',
-#         'PASSWORD':'admin',
-#         'HOST':'127.0.0.1',
-#         'PORT':'3306'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spoturstay_db',
+        'USER': 'django_user',
+        'PASSWORD':'admin',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 CORS_ALLOW_ALL_ORIGINS = True  # You can restrict this by specifying allowed origins like below:
 # CORS_ALLOWED_ORIGINS = [
