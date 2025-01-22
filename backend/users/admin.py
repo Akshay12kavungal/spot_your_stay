95470636d7e8b6afc6935768b079916478301276
Akshay.k <akshaykvl90@gmail.com>
2025-01-22 19:46:11 +0530

    admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -7,3 +7,11 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'phone_number', 'is_staff')
     list_filter = ('is_staff', 'is_superuser')
     search_fields = ('username', 'email', 'phone_number')
+
+
+# your_app/admin.py
+from django.contrib import admin
+
+admin.site.site_header = "Spot Your Stay"
+admin.site.site_title = "Spot Your Stay"
+admin.site.index_title = "Welcome to Your Spot Your Stay Admin Portal"
